2774a9c18114dbf88da96ef3e3b4a6f2efc74680
xaunlam <xaunlam@gmail.com>
2026-01-08 16:35:22 +0700

    Fix error ci file

--- doctorloan-api/src/Core/DoctorLoan.Application/Models/Settings/PayooConfiguration.cs
+++ doctorloan-api/src/Core/DoctorLoan.Application/Models/Settings/PayooConfiguration.cs
@@ -1,1 +1,8 @@
-﻿namespace DoctorLoan.Application.Models.Settings;public class PayooConfiguration{    public string CheckSumKey { get; set; }    public string SandboxIp { get; set; }    public string ProductionIp { get; set; }}
+﻿namespace DoctorLoan.Application.Models.Settings;
+
+public class PayooConfiguration
+{
+    public string CheckSumKey { get; set; }
+    public string SandboxIp { get; set; }
+    public string ProductionIp { get; set; }
+}
